fe34d70b2cd4e75c48e32597224ab4d8c9d7854b
MVIC-lab <ngoumouam@gmail.com>
2021-09-27 19:59:39 +0200

    functions nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,19 @@
 #include "main.h"
-/*print_alphabet_x10 - prints alphabet letters ten times*/
+/**
+*print_alphabet_x10 - prints alphabet letters
+* ten times
+*desc: The function uses loop to operate
+*Return: no return
+*/
 void print_alphabet_x10(void)
 {
-int number = 0;
+int counter = 0;
 int letter;
-while (number++ <= 9)
+while (counter++ <= 9)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 
 _putchar(10);
 }
-
 }
