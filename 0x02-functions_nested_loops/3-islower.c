92dcdfc5d05171dfe760d0a4ff7c85cde74bef2d
MVIC-lab <ngoumouam@gmail.com>
2021-09-27 20:24:48 +0200

    functions nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*C Program to check the character is lowercase
+*_islower - controls if a character is in lowerce
+*/
+int _islower(int ch)
+{
+if (ch >= 65 && ch <= 90)
+return (1);
+
+return (0);
+}
