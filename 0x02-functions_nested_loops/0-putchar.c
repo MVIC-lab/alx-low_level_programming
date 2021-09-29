345d77addc9a3795b7b9a79696e5ac8791de0006
MVIC-lab <ngoumouam@gmail.com>
2021-09-29 15:35:41 +0200

    functions nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include "main.h"
 
 /**
 *main - function prints text as output
@@ -6,15 +7,14 @@
 */
 int main(void)
 {
-        char a[10] = "_putchar";
+	char school[10] = "_putchar";
 
-        int j;
+	int i;
 
-        for (j=0;j<8;j++)
-        {
-                _putchar(a[j]);
-        }
-        _putchar(10);
-        return (0);
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar(10);
+	return (0);
 }
-
